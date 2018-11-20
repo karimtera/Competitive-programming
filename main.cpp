772606c43ee73a974348b9e7fd1d4fb3846dbd67
Karim Abdelhady Tera <42269309+karimtera@users.noreply.github.com>
2018-11-20 13:00:58 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,a[int(1e5+9)];
+int tail[int(1e5+9)];
+
+int lis_nlogn(int A[],int s){ // s is the size of array A
+    // this function finds the lis (strictly increasing)
+    if(s==0) return 0;
+    int l=0;
+    tail[l++]=A[0];
+    for(int i=1;i<s;i++){
+        if(A[i]>tail[l-1]) tail[l++]=A[i];
+        else{
+            int idx=find(tail,tail+l,A[i])-tail;
+            if(idx!=l) continue;
+            else{
+                idx=upper_bound(tail,tail+l,A[i])-tail;
+                tail[idx]=A[i];
+            }
+        }
+    }
+    return l;
+}
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    printf("%d",lis_nlogn(a,n));
+    return 0;
+}
