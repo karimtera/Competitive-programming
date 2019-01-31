7249550cc9733d4c79982afb79d084d0c1c44f94
Karim Abdelhady Tera <42269309+karimtera@users.noreply.github.com>
2019-01-31 21:35:30 +0200

    Create set with modified cmp.cpp

---
+++ set with modified cmp.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stack>
+#include <map>
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <math.h>
+#include <vector>
+#include <set>
+#include <queue>
+#include <climits>
+#include <unordered_map>
+#include <iterator> 
+#include <bitset>
+#include <complex>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define mp make_pair
+#define inf32 INT_MAX
+#define inf64 LLONG_MAX
+#define PI acos(-1)
+#define cos45 cos(PI/4)
+#define ld long double
+#define inf 1000000
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#pragma GCC optimize ("O3")
+//#define x first
+//#define y second
+struct cmp {
+	bool operator() (const pii &a, const pii &b) {
+		if (a.first != b.first) {
+			return a.first > b.first;
+		}
+		return a.second > b.second;
+	}
+};
+multiset<pii, cmp> lrg;
+
+int main()
+{
+	
+  	return 0;
+}
