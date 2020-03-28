2b1ca9f109432d5686edaf25d15be769dffd69f6
Karim Abdelhady Tera <42269309+karimtera@users.noreply.github.com>
2020-03-28 07:14:41 +0200

    Add files via upload

---
+++ A629.cpp
@@ -0,0 +1,36 @@
+//    _____ _     _                _           _     _ _     _     
+//    \_   \ |_  (_)___  __      _| |__   __ _| |_  (_) |_  (_)___ 
+//     / /\/ __| | / __| \ \ /\ / / '_ \ / _` | __| | | __| | / __|
+//  /\/ /_ | |_  | \__ \  \ V  V /| | | | (_| | |_  | | |_  | \__ \
+//  \____/  \__| |_|___/   \_/\_/ |_| |_|\__,_|\__| |_|\__| |_|___/
+//                                                                 
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+ll div_floor(ll a, ll b) {
+	// use when ret <= a/b, (a and b can be -ve)
+	return a / b - (((a ^ b) < 0) and a % b);
+}
+ll div_ceil(ll a, ll b) { 
+	// use when ret >= a/b, (a and b can be -ve)
+	return a / b + (((a ^ b) >= 0) and a % b);
+}
+const int sz=1e6+9;
+
+int main(){
+	int t,a,b;
+	cin>>t;
+	while(t--){
+		cin>>a>>b;
+		cout<<((b-a%b)==b?0:b-a%b)<<'\n';
+	}
+	return 0;
+}
